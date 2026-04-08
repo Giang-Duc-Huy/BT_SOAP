813dc5b92a7769d207e2b790baa0f68899f83337
Giang-Duc-Huy <giangduchuy537@gmail.com>
2026-04-08 16:52:14 +0700

    add files

---
+++ BT_B2/CalculatorWinForm/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CalculatorWinForm
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Num1:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(100, 27);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(150, 22);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Num2:";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(100, 67);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(150, 22);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(30, 110);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 30);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(120, 110);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 30);
+            this.btnSubtract.TabIndex = 5;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(210, 110);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 30);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(300, 110);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 30);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(30, 160);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(52, 17);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "Result: ";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(420, 210);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculator SOAP Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ BT_B2/CalculatorWinForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+using CalculatorWinForm.CalculatorService; // namespace của service reference
+
+namespace CalculatorWinForm
+{
+    public partial class Form1 : Form
+    {
+        private CalculatorWslmplClient serviceClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serviceClient = new CalculatorWslmplClient(); // tạo proxy client
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CallService("add");
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            CallService("subtract");
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            CallService("multiply");
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            CallService("divide");
+        }
+
+        private void CallService(string operation)
+        {
+            try
+            {
+                int num1 = int.Parse(txtNum1.Text);
+                int num2 = int.Parse(txtNum2.Text);
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "add":
+                        result = serviceClient.add(num1, num2);
+                        break;
+                    case "subtract":
+                        result = serviceClient.subtract(num1, num2);
+                        break;
+                    case "multiply":
+                        result = serviceClient.multiply(num1, num2);
+                        break;
+                    case "divide":
+                        result = serviceClient.divide(num1, num2);
+                        break;
+                }
+
+                lblResult.Text = "Result: " + result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
